af61d80a7b58b6d4043a090fe4d196ba14f602e8
Michelle <michelle.chlin15@gmail.com>
2020-02-14 00:52:54 -0500

    update

---
+++ _posts/2019-02-13-1Days-of-Code-Day1-10.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "1DaysOfChallenge-Day1-10"
+categories:
+  "year-archive"
+---
+
+test test
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "1DaysOfChallenge-Day1-10",
+        "excerpt":"test test  ","categories": ["year-archive"],
+        "tags": [],
+        "url": "http://localhost:4000/year-archive/1Days-of-Code-Day1-10/",
+        "teaser":null},{
+        "title": "100DaysOfChallenge-Day1-10",
+        "excerpt":"This is my main goals:  ","categories": ["year-archive"],
+        "tags": [],
+        "url": "http://localhost:4000/year-archive/100Days-of-Code-Day1-10/",
+        "teaser":null}]
