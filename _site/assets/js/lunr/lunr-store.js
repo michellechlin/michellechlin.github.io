1c44aa9d9f3aeb058834f31cc8f9928fba72c962
Michelle <michelle.chlin15@gmail.com>
2020-02-16 00:17:29 -0500

    Update title of the post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,7 +4,7 @@ var store = [{
         "tags": [],
         "url": "http://localhost:4000/year-archive/1Days-of-Code-Day1-10/",
         "teaser":null},{
-        "title": "Hundred Days Of Code - Day1-10",
+        "title": "Hundred Days Of Code - The First Week",
         "excerpt":"After quitting my job from R&amp;D scientist, I become a stay-at-home mom. Although I have more time with my child, the depressed feeling comes and goes a being a full-time mom is lack of appreciation or a missing sense of accomplishment. So, I commit myself to take 100 Days of...","categories": ["Web design"],
         "tags": ["100DaysOfCode","CSS","HTML"],
         "url": "http://localhost:4000/web%20design/100Days-of-Code-Day1-10/",
